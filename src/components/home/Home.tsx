93595b4213309c68e623d920bc113a081a5f16fc
danielaleksandrov <crunchyhotline@gmail.com>
2021-04-10 13:45:38 -0700

    'home-vertically-resp'

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -7,10 +7,12 @@ function Home() {
 
       <div className="red-dice-bg" />
       <div className="triangle-green-container" />
-      
+
       <div className="home-green-container">
-        <h1>Front End Developer</h1>
-        <p>Hi. I’m Daniel, a Front-end Developer with proficient skills to make well designed websites with React.</p>
+        <div>
+          <h1>Front End Developer</h1>
+          <p>Hi. I’m Daniel, a Front-end Developer with proficient skills to make well designed websites with React.</p>
+        </div>
       </div>
     </div>
   )
